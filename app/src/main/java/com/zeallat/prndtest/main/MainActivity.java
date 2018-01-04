201d9a1e06fc4b21552693428f8bbd3eace6ef16
HoJunLee <zeallat94@gmail.com>
2018-01-05 00:49:47 +0900

    Fix bug - show Vector drawable

--- app/src/main/java/com/zeallat/prndtest/main/MainActivity.java
+++ app/src/main/java/com/zeallat/prndtest/main/MainActivity.java
@@ -3,6 +3,7 @@ package com.zeallat.prndtest.main;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.zeallat.prndtest.R;
 import com.zeallat.prndtest.base.BaseActivityConfig;
 import com.zeallat.prndtest.base.BaseViewActivity;
 
@@ -11,7 +12,7 @@ public class MainActivity extends BaseViewActivity<MainContract.Presenter> imple
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         new MainPresenter(this);
-        setContentView(null);
+        setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
     }
 
