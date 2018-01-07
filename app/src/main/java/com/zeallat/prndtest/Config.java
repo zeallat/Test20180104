4a044c29267aaa7419a37e2b2b1222d332996442
HoJunLee <zeallat94@gmail.com>
2018-01-08 00:32:00 +0900

    Remove unuse config

--- app/src/main/java/com/zeallat/prndtest/Config.java
+++ app/src/main/java/com/zeallat/prndtest/Config.java
@@ -10,22 +10,5 @@ public class Config {
      * true로 설정시 모든 액티비티가 세로화면으로 고정된다.
      */
     public static final boolean IS_SCREEN_ORIENTATION_PORTRAIT_REQUIRED = false;
-
-    /**
-     * Firebase realtime database path
-     */
-    public enum FirebaseDatabaseRef {
-        USER("users");
-        private String path;
-
-        FirebaseDatabaseRef(String path) {
-            this.path = path;
-        }
-
-        public String getPath() {
-            return path;
-        }
-    }
-
     public static final String API_DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
 }
